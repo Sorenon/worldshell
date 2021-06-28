b74322cb29870885628fbef99cdf308baae55183
Sorenon <sorenonandstuffs@gmail.com>
2021-06-29 00:29:24 +0100

    fix dedicated server issue

--- src/main/java/net/snakefangox/worldshell/WorldShellMain.java
+++ src/main/java/net/snakefangox/worldshell/WorldShellMain.java
@@ -3,26 +3,23 @@ package net.snakefangox.worldshell;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.bullet.Bullet;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.RenderLayer;
+import net.minecraft.command.argument.DimensionArgumentType;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.biome.source.BiomeAccessType;
+import net.minecraft.world.biome.source.DirectBiomeAccessType;
 import net.minecraft.world.biome.source.FixedBiomeSource;
 import net.minecraft.world.dimension.DimensionOptions;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.snakefangox.worldshell.kevlar.FakeApp;
-import net.snakefangox.worldshell.kevlar.PhysicsWorld;
-import net.snakefangox.worldshell.mixinextras.WorldExt;
 import net.snakefangox.worldshell.storage.EmptyChunkGenerator;
 import net.snakefangox.worldshell.storage.ShellStorageData;
 import net.snakefangox.worldshell.transfer.ShellTransferHandler;
@@ -32,6 +29,7 @@ import net.snakefangox.worldshell.world.ShellStorageWorld;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.OptionalLong;
 import java.util.function.Supplier;
 
 public class WorldShellMain implements ModInitializer {
@@ -40,6 +38,28 @@ public class WorldShellMain implements ModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	public static final RegistryKey<World> STORAGE_DIM = RegistryKey.of(Registry.WORLD_KEY, new Identifier(MODID, "shell_storage"));
+
+	public static final RegistryKey<DimensionType> STORAGE_DIM_TYPE_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY, new Identifier(MODID, "shell_storage_type"));
+	public static final DimensionType STORAGE_TIME_TYPE = DimensionType.create(
+			OptionalLong.of(6000),
+			true,
+			false,
+			false,
+			false,
+			1,
+			false,
+			false,
+			false,
+			false,
+			false,
+			-256,
+			512,
+			512,
+			DirectBiomeAccessType.INSTANCE,
+			new Identifier("minecraft:infiniburn_overworld"),
+			DimensionType.OVERWORLD_ID,
+			0.1f
+	);
 
 	public static ServerWorld getStorageDim(MinecraftServer server) {
 		return server.getWorld(WorldShellMain.STORAGE_DIM);
@@ -68,7 +88,7 @@ public class WorldShellMain implements ModInitializer {
 
 	public void registerShellStorageDimension(MinecraftServer server) {
 		Supplier<DimensionType> typeSupplier = () -> server.getRegistryManager().get(Registry.DIMENSION_TYPE_KEY)
-				.get(RegistryKey.of(Registry.DIMENSION_TYPE_KEY, new Identifier(MODID, "shell_storage_type")));
+				.get(STORAGE_DIM_TYPE_KEY);
 		ChunkGenerator chunkGenerator = new EmptyChunkGenerator(new FixedBiomeSource(server.getRegistryManager()
 				.get(Registry.BIOME_KEY).get(BiomeKeys.THE_VOID)));
 		DimensionOptions options = new DimensionOptions(typeSupplier, chunkGenerator);
